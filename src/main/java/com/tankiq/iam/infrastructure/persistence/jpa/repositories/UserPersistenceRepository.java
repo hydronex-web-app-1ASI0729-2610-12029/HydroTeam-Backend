ea4355eecc3ebc939a6094250ea23d6502dc46fc
Bruno <brunorodolfomontalvan@gmail.com>
2026-06-19 16:14:46 -0500

    feat(iam): add findByEmail and existsByEmail to UserPersistenceRepository

--- src/main/java/com/tankiq/iam/infrastructure/persistence/jpa/repositories/UserPersistenceRepository.java
+++ src/main/java/com/tankiq/iam/infrastructure/persistence/jpa/repositories/UserPersistenceRepository.java
@@ -3,7 +3,10 @@ package com.tankiq.iam.infrastructure.persistence.jpa.repositories;
 import com.tankiq.iam.infrastructure.persistence.jpa.entities.UserPersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserPersistenceRepository extends JpaRepository<UserPersistenceEntity, Long> {
+    Optional<UserPersistenceEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
