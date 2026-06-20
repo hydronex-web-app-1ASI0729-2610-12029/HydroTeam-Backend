011932dffac41b54376c656604c4ee7f130fe42d
Bruno <brunorodolfomontalvan@gmail.com>
2026-06-20 10:08:35 -0500

    feat(iam): add associatedAt to CreateUserBuildingResource

--- src/main/java/com/tankiq/iam/interfaces/rest/resources/CreateUserBuildingResource.java
+++ src/main/java/com/tankiq/iam/interfaces/rest/resources/CreateUserBuildingResource.java
@@ -3,10 +3,13 @@ package com.tankiq.iam.interfaces.rest.resources;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDateTime;
+
 public record CreateUserBuildingResource(
         @NotNull Long userId,
         @NotNull Long buildingId,
         @NotBlank String role,
-        String apartmentNumber
+        String apartmentNumber,
+        LocalDateTime associatedAt
 ) {
 }
