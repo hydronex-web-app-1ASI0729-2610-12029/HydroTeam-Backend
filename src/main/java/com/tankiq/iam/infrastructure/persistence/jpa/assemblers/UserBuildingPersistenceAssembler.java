811793151dfebe70b58e459e4d6ec52bbd23a412
Bruno <brunorodolfomontalvan@gmail.com>
2026-06-20 09:48:58 -0500

    feat(iam): add UserBuildingPersistenceAssembler

---
+++ src/main/java/com/tankiq/iam/infrastructure/persistence/jpa/assemblers/UserBuildingPersistenceAssembler.java
@@ -0,0 +1,30 @@
+package com.tankiq.iam.infrastructure.persistence.jpa.assemblers;
+
+import com.tankiq.iam.domain.model.aggregates.UserBuilding;
+import com.tankiq.iam.infrastructure.persistence.jpa.entities.UserBuildingPersistenceEntity;
+
+public final class UserBuildingPersistenceAssembler {
+
+    private UserBuildingPersistenceAssembler() {
+    }
+
+    public static UserBuilding toDomainFromPersistence(UserBuildingPersistenceEntity entity) {
+        return new UserBuilding(
+                entity.getId(),
+                entity.getUserId(),
+                entity.getBuildingId(),
+                entity.getRole(),
+                entity.getApartmentNumber()
+        );
+    }
+
+    public static UserBuildingPersistenceEntity toPersistenceFromDomain(UserBuilding userBuilding) {
+        var entity = new UserBuildingPersistenceEntity();
+        entity.setId(userBuilding.getId());
+        entity.setUserId(userBuilding.getUserId());
+        entity.setBuildingId(userBuilding.getBuildingId());
+        entity.setRole(userBuilding.getRole());
+        entity.setApartmentNumber(userBuilding.getApartmentNumber());
+        return entity;
+    }
+}
