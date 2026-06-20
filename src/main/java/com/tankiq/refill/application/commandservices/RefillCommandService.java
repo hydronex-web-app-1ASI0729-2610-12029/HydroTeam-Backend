23e26c4ea9d35fd9e0e34bbc43394fc63030abd0
juancarlosorosco59 <u202414840@upc.edu.pe>
2026-06-20 11:34:16 -0500

    feat(refill): add update refill command handler contract

--- src/main/java/com/tankiq/refill/application/commandservices/RefillCommandService.java
+++ src/main/java/com/tankiq/refill/application/commandservices/RefillCommandService.java
@@ -3,10 +3,12 @@ package com.tankiq.refill.application.commandservices;
 import com.tankiq.refill.domain.model.aggregates.Refill;
 import com.tankiq.refill.domain.model.commands.CreateRefillCommand;
 import com.tankiq.refill.domain.model.commands.DeleteRefillCommand;
+import com.tankiq.refill.domain.model.commands.UpdateRefillCommand;
 import com.tankiq.shared.application.result.ApplicationError;
 import com.tankiq.shared.application.result.Result;
 
 public interface RefillCommandService {
     Result<Refill, ApplicationError> handle(CreateRefillCommand command);
     Result<Void, ApplicationError> handle(DeleteRefillCommand command);
+    Result<Refill, ApplicationError> handle(UpdateRefillCommand command);
 }
